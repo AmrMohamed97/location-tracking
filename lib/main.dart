fa43082a4dbeb7a15a4415d87a87ecacfa2c2fe5
amrMohamed-pureSoft <amr.mohamed@pure-soft.com>
2025-04-27 16:00:20 +0300

    edit tracking page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'dart:async';
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:ios_android_background/tracking_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,98 +20,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TrackingPage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  static const platform = MethodChannel("myLocationTracking");
-  static const eventChannel = EventChannel("myLocationUpdates");
-  StreamSubscription<dynamic>? _locationSubscription;
-  String _location = "Waiting for location...";
-  bool _serviceStarted = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _startListeningToLocationUpdates();
-  }
-
-  Future<void> _startService() async {
-    try {
-        await platform.invokeMethod('startService');
-      setState(() {
-        _serviceStarted = true;
-      });
-      // print("Service started: $result");
-    } on PlatformException catch (e) {
-      // print("Failed to start service: ${e.message}");
-      setState(() {
-        _serviceStarted = false;
-      });
-      // يمكنك هنا عرض رسالة للمستخدم تطلب منه إعطاء الصلاحيات
-    }
-  }
-
-  void _startListeningToLocationUpdates() {
-    _locationSubscription = eventChannel.receiveBroadcastStream().listen(
-      (dynamic location) {
-        setState(() {
-          // print("Received location update: $location");
-          _location = location.toString();
-        });
-      },
-      onError: (dynamic error) {
-        // print("Error receiving location updates: $error");
-      },
-    );
-  }
-
-  @override
-  void dispose() {
-    _locationSubscription?.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: MaterialButton(
-                onPressed: _serviceStarted
-                    ? null
-                    : () async {
-                        await _startService();
-                      },
-                child:
-                    Text(_serviceStarted ? 'Service Running' : 'Start Service'),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              "Current Location: $_location",
-              textAlign: TextAlign.center,
-              style: const TextStyle(fontSize: 16),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
---
+++ lib/tracking_page.dart
@@ -0,0 +1,95 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class TrackingPage extends StatefulWidget {
+  const TrackingPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<TrackingPage> createState() => _TrackingPageState();
+}
+
+class _TrackingPageState extends State<TrackingPage> {
+  static const platform = MethodChannel("myLocationTracking");
+  static const eventChannel = EventChannel("myLocationUpdates");
+  StreamSubscription<dynamic>? _locationSubscription;
+  String _location = "Waiting for location...";
+  bool _serviceStarted = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _startListeningToLocationUpdates();
+  }
+
+  Future<void> _startService() async {
+    try {
+        await platform.invokeMethod('startService');
+      setState(() {
+        _serviceStarted = true;
+      });
+      // print("Service started: $result");
+    } on PlatformException catch (e) {
+      // print("Failed to start service: ${e.message}");
+      setState(() {
+        _serviceStarted = false;
+      });
+      // يمكنك هنا عرض رسالة للمستخدم تطلب منه إعطاء الصلاحيات
+    }
+  }
+
+  void _startListeningToLocationUpdates() {
+    _locationSubscription = eventChannel.receiveBroadcastStream().listen(
+      (dynamic location) {
+        setState(() {
+          // print("Received location update: $location");
+          _location = location.toString();
+        });
+      },
+      onError: (dynamic error) {
+        // print("Error receiving location updates: $error");
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _locationSubscription?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: MaterialButton(
+                onPressed: _serviceStarted
+                    ? null
+                    : () async {
+                        await _startService();
+                      },
+                child:
+                    Text(_serviceStarted ? 'Service Running' : 'Start Service'),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              "Current Location: $_location",
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
